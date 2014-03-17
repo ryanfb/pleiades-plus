0665441c95b44c77c071ca779f626bde663f9d00
Ryan Baumann <ryan.baumann@gmail.com>
2014-03-17 16:43:42 -0400

    Add update-cron.sh script

---
+++ update-cron.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# needed for ssh-agent auth under cron on OS X
+declare -x SSH_AUTH_SOCK=$( find /tmp/launch-*/Listeners -user $(whoami) -type s | head -1 )
+
+rm -v *.csv.gz *.csv
+rm -rfv /tmp/geonames
+git checkout master
+git pull
+./create_pleiades_plus
+git add data/pleiades-plus.csv
+git commit -m "$(date '+%Y-%m-%d') pleiades-plus.csv update"
+git checkout capgrids
+git pull
+git merge -s recursive -Xtheirs master
+./create_pleiades_plus
+git add data/pleiades-plus.csv
+git commit -m "$(date '+%Y-%m-%d') pleiades-plus.csv capgrids update"
+git push
+git checkout master
